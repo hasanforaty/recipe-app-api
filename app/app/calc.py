32f0ce4e745ea14bd14296ae4ca4200a431d59fc
hasan foraty <hasanforaty@gmail.com>
2024-01-14 13:00:46 +0330

    adding test

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n2 - n1
---
+++ app/app/test.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+from app.app import calc
+
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        """Test adding two numbers"""
+        self.assertEqual(calc.add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting two numbers"""
+        self.assertEqual(calc.subtract(8, 3), 5)
