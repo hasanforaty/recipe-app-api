19278167c46a88480498d81b7e933258c4e3119e
hasan foraty <hasanforaty@gmail.com>
2024-01-16 13:29:03 +0330

    Add Spectacular + Swagger

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,13 @@
 from django.test import SimpleTestCase
 from app import calc
+from django.urls import reverse
+from django.test import Client
 
 
 class CalcTests(SimpleTestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_add_numbers(self):
         """Test adding two numbers"""
         self.assertEqual(calc.add(3, 8), 11)
@@ -10,3 +15,15 @@ class CalcTests(SimpleTestCase):
     def test_subtract_numbers(self):
         """Test subtracting two numbers"""
         self.assertEqual(calc.subtract(8, 3), 5)
+
+    def test_schema_success(self):
+        """Test schema url for success"""
+        url = reverse("api-schema")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_docs_success(self):
+        """Test schema url for success"""
+        url = reverse("api-docs")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
