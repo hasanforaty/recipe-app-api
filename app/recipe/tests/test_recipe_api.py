0d64daa8a4758ef9958b995e410e6e251908f0ae
hasan foraty <hasanforaty@gmail.com>
2024-01-17 13:24:37 +0330

    adding serializers for recipe-detail + add user to creating recipe detail

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -9,8 +9,14 @@ from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
 from core.models import Recipe
 from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeDetailSerializer
 
 RECIPE_URL = reverse('recipe:recipe-list')
+
+
+def detail_url(recipe_id):
+    """create and return a recipe detail URL"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 
 def create_recipe(user, **params):
@@ -24,6 +30,12 @@ def create_recipe(user, **params):
     }
     defaults.update(params)
     return Recipe.objects.create(user=user, **defaults)
+
+
+def create_user(**param):
+    """Create and return new User"""
+    user = get_user_model().objects.create_user(**param)
+    return user
 
 
 class PublicRecipeApiTests(TestCase):
@@ -45,10 +57,7 @@ class PrivateRecipeApiTest(TestCase):
     """Test the authorized user recipe API"""
 
     def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            email='test@example.com',
-            password='password'
-        )
+        self.user = create_user(email='example@example.com', password='12345')
         self.client = APIClient()
         self.client.force_authenticate(self.user)
 
@@ -64,10 +73,7 @@ class PrivateRecipeApiTest(TestCase):
 
     def test_recipes_limited_to_user(self):
         """Test list of recipes is limited to authenticated user"""
-        other_user = get_user_model().objects.create_user(
-            email='other@example.com',
-            password='1234'
-        )
+        other_user = create_user(email='other@example.com', password='otherPassword')
         create_recipe(user=other_user, )
         create_recipe(user=self.user)
         res = self.client.get(RECIPE_URL)
@@ -75,3 +81,65 @@ class PrivateRecipeApiTest(TestCase):
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """Test retrieving recipe details"""
+        recipe = create_recipe(user=self.user, )
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_recipe(self):
+        """Test creating Recipe successfully"""
+        payload = {
+            'title': 'sample recipe',
+            "time_minutes": 22,
+            "price": Decimal('5.25'),
+            'description': 'sample description',
+            'link': 'https://example.com/recipe.pdf',
+        }
+
+        res = self.client.post(RECIPE_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.filter(id=res.data['id']).first()
+        for k, v in payload.items():
+            self.assertEqual(getattr(recipe, k), v)
+        self.assertEqual(recipe.user, self.user)
+
+    def test_partial_update_recipe(self):
+        """Test partial update of recipe details"""
+        original_link = 'https://example.com/recipe.pdf'
+        recipe = create_recipe(user=self.user, link=original_link)
+        payload = {'title': 'new recipe title'}
+        url = detail_url(recipe.id)
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(recipe.link, original_link)
+        self.assertEqual(recipe.user, self.user)
+
+    def test_full_update(self):
+        """test full update of recipe """
+        recipe = create_recipe(user=self.user,
+                               title='sample recipe title',
+                               description='sample description',
+                               link='https://example.com/recipe.pdf'
+                               )
+        payload = {
+            'title': 'new recipe title',
+            'description': 'new recipe description',
+            'link': 'https://example.com/2recipe.pdf',
+            'time_minutes': 10,
+            'price': Decimal('2.50')
+        }
+        url = detail_url(recipe.id)
+        res = self.client.put(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        recipe.refresh_from_db()
+        for key, value in payload.items():
+            self.assertEqual(getattr(recipe, key), value)
+        self.assertEqual(recipe.user, self.user)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ View for recipe API
 from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 from core.models import Recipe
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
@@ -13,7 +13,7 @@ from rest_framework.settings import api_settings
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View to manage recipe APIs"""
-    serializer_class = RecipeSerializer
+    serializer_class = RecipeDetailSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     permission_classes = (IsAuthenticated,)
     queryset = Recipe.objects.all()
@@ -22,3 +22,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return appropriate serializer base on action of user"""
+        if self.action == 'list':
+            return RecipeSerializer
+        else:
+            return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe """
+        serializer.save(user=self.request.user)
+
